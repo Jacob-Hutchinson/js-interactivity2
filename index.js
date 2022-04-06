55e4645a6a940868d1068abb4c3044ab0906a95c
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-04-06 17:20:40 -0600

    finished the secound half

--- index.js
+++ index.js
@@ -30,14 +30,23 @@ form.addEventListener('submit', addMovie)
 
 const deleteMovie = (event) => {
     event.target.parentNode.remove() 
-    message.textContent = `Movie deleted!`
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`
+    revalMessage()
 }
 
 const crossOffMovie = (event) => {
     event.target.classList.toggle('checked') 
     if(event.target.classList.contains('checked')){
-        message.textContent = 'movie watched!'
+        message.textContent = `${event.target.textContent} watched!`
     }else{
-        message.textContent = 'movie added back'
+        message.textContent = `${event.target.textContent} added back`
     }
+    revalMessage()
+}
+
+const revalMessage = () => {
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000);
+    message.classList.remove('hide')
 }
